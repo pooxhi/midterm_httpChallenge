592c38b9fed94c0d75dc23f88208dbc85bb98b8e
pooxhi <rak.mariano6262@gmail.com>
2025-02-09 23:06:19 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpclientService } from './httpclient.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'http_challenge';
+  title = 'clientprg';
+  httpusers: any;
+
+  constructor(private httpclientList: HttpclientService) {
+    this.httpclientList.getUsersRemotely().subscribe((data: any) => {
+      this.httpusers = data;
+    });
+  }
 }
